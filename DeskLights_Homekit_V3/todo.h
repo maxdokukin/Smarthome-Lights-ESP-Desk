423ecd7c191bd9f7d1524f64c7934408a140341c
xewe <xewe@xewes-MacBook-Air.local>
2023-11-11 01:14:54 -0800

    Working on smooth brightness

--- DeskLights_Homekit_V3/todo.h
+++
@@ -1,7 +0,0 @@
-/*
-* Add smooth color transition
-* Add smooth brightness transition
-* Alexa support 
-* Fix memory leak at void write(client_context_t *context, byte *data, int data_size) {
-x Change var names 
-*/
--- README.md
+++
@@ -1,4 +0,0 @@
-# Smarthome-Lights-ESP
-
-Connecting address LED to Homekit and Alexa smart homes.
-LED strip has 2 modes: solid color, and perlin fade (pretty waves)
